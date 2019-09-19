1f51ec9bb18b2ef989ce370b2e7623bef29c0b4d
aeneasr <aeneas@ory.sh>
2019-09-19 11:06:00 +0200

    Force trailing slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,5 +120,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `gatsby-plugin-force-trailing-slashes`,
   ],
 }
